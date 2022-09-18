c1d7473fb43d0d0fd65673e76a82bbe7d04d0dbb
kweku moses <kweku.moses@hotmail.com>
2022-09-18 18:10:22 +0200

    Change src to tsrc

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.get('/:url', (req, res) => {
   // console.log(req.params.url);
   //  Get the req.route.path  and send that data forward to the viewer.
   // let android_src = `https://s3.eu-west-2.amazonaws.com/product.baetes.com/Limitato_Slipin_DarkGreen_${req.params.url}.glb`;
-  let test = req.query.test
+  let tsrc = req.query.tsrc
   let product = req.query.product
   let button_text = req.query.button_text
   let button_border = req.query.button_border
@@ -87,11 +87,11 @@ app.get('/:url', (req, res) => {
     }
   })
 
-  console.log(width)
+  console.log(tsrc)
 
   res.render('index', {
     root: __dirname,
-    Src: test,
+    tsrc: tsrc,
     button_text: button_text,
     sizes_used: sizes_used,
     colors_used: colors_used,
