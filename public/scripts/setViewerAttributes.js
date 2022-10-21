5c89f5056c1cd4d791c816f6663d935c4ec2dcc5
kweku moses <kweku.moses@hotmail.com>
2022-10-21 18:06:33 +0200

    Fetch before set

--- public/scripts/setViewerAttributes.js
+++ public/scripts/setViewerAttributes.js
@@ -1,8 +1,10 @@
-const setViewerAttributes = async (product) => {
+const setViewerAttributes = (product) => {
+  fetchUserData(product)
+
   document.getElementById('the-viewer').setAttribute('src', product + '.glb')
   document
     .getElementById('the-viewer')
     .setAttribute('ios-src', product + '.usdz')
 
-  await fetchUserData(product)
+  console.log('set')
 }
