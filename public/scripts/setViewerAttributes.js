5d9148779dc442a32d053ce9c6f7d45c9dc01ef3
kweku moses <kweku.moses@hotmail.com>
2022-10-21 17:51:33 +0200

    make setViewerAttributes asyn

--- public/scripts/setViewerAttributes.js
+++ public/scripts/setViewerAttributes.js
@@ -1,8 +1,8 @@
-const setViewerAttributes = (product) => {
-  fetchUserData(product)
-
-  document.getElementById('the-viewer').setAttribute('src', product + '.glb')
-  document
-    .getElementById('the-viewer')
-    .setAttribute('ios-src', product + '.usdz')
+const setViewerAttributes = async (product) => {
+  await fetchUserData(product).then(() => {
+    document.getElementById('the-viewer').setAttribute('src', product + '.glb')
+    document
+      .getElementById('the-viewer')
+      .setAttribute('ios-src', product + '.usdz')
+  })
 }
