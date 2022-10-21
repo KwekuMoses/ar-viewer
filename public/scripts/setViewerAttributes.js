705b7ac9a29f24e0b36a6ea19ac37e3e8e8d9174
kweku moses <kweku.moses@hotmail.com>
2022-10-21 17:54:59 +0200

    Fetch after setting userData

--- public/scripts/setViewerAttributes.js
+++ public/scripts/setViewerAttributes.js
@@ -1,8 +1,8 @@
 const setViewerAttributes = async (product) => {
-  await fetchUserData(product).then(() => {
-    document.getElementById('the-viewer').setAttribute('src', product + '.glb')
-    document
-      .getElementById('the-viewer')
-      .setAttribute('ios-src', product + '.usdz')
-  })
+  document.getElementById('the-viewer').setAttribute('src', product + '.glb')
+  document
+    .getElementById('the-viewer')
+    .setAttribute('ios-src', product + '.usdz')
+
+  await fetchUserData(product)
 }
