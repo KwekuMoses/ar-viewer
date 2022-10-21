750d11a5b2238745b2542fcb68394d733483b21e
kweku moses <kweku.moses@hotmail.com>
2022-10-21 18:10:31 +0200

    Call fetchUserData before setViewerAttributes

--- public/scripts/setViewerAttributes.js
+++ public/scripts/setViewerAttributes.js
@@ -1,6 +1,4 @@
 const setViewerAttributes = (product) => {
-  fetchUserData(product)
-
   document.getElementById('the-viewer').setAttribute('src', product + '.glb')
   document
     .getElementById('the-viewer')
