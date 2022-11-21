c05aa87f31c29ba1d8752ab7146e9731beab8aa7
kweku moses <kweku.moses@hotmail.com>
2022-11-21 23:45:45 +0100

    Test

--- public/scripts/fetchUserData.js
+++ public/scripts/fetchUserData.js
@@ -1,34 +1,32 @@
-let viewer = document.getElementById('the-viewer')
-let elements = document.getElementsByClassName('item-drop')
+// let viewer = document.getElementById('the-viewer')
+// let elements = document.getElementsByClassName('item-drop')
 
-Array.from(elements).forEach(function (element) {
-  // element.addEventListener('click', activateAR)
-})
+// Array.from(elements).forEach(function (element) {
+//   element.addEventListener('click', activateAR)
+// })
 
-const activateAR = (button) => {
-  // viewer.activateAR()
-}
+// const activateAR = () => {
+//   viewer.activateAR()
+// }
 
 const fetchUserData = async (e, product, color) => {
-  // const request = await fetch('https://ipinfo.io/json?token=48c7527aaa89bb')
-  // const jsonResponse = await request.json()
-  // let city = jsonResponse.city
-  // let country = jsonResponse.country
-  // let userData = jsonResponse
-  // const data = { city, country, product, userData }
+  const request = await fetch('https://ipinfo.io/json?token=48c7527aaa89bb')
+  const jsonResponse = await request.json()
+  let city = jsonResponse.city
+  let country = jsonResponse.country
+  let userData = jsonResponse
+  const data = { city, country, product, userData }
 
-  // const options = {
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/json'
-  //   },
-  //   body: JSON.stringify(data)
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
 
-  // }
+  }
 
-  // fetch('/', options).then((response) => {
-  // })
-  viewer.activateAR()
+  fetch('/', options)
 
   // try {
   //   const fetchResponse = await fetch(`/`, options);
