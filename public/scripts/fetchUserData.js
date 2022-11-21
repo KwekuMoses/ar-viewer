6a366645ccef66d8c5cad51457c2c72fed2535ee
kweku moses <kweku.moses@hotmail.com>
2022-11-21 22:41:05 +0100

    Test

--- public/scripts/fetchUserData.js
+++ public/scripts/fetchUserData.js
@@ -1,11 +1,13 @@
+let viewer = document.getElementById('the-viewer')
+let elements = document.getElementsByClassName('item-drop')
+
 const fetchUserData = async (product) => {
   const request = await fetch('https://ipinfo.io/json?token=48c7527aaa89bb')
   const jsonResponse = await request.json()
   let city = jsonResponse.city
   let country = jsonResponse.country
   let userData = jsonResponse
-  let viewer = document.getElementById('the-viewer')
-  let elements = document.getElementsByClassName('item-drop')
+
   const data = { city, country, product, userData }
   let dataString = json.stringify(data)
   const options = {
@@ -26,11 +28,13 @@ const fetchUserData = async (product) => {
   } catch (e) {
     return e;
   }
-  const activateAR = () => {
-    viewer.activateAR()
-  }
-  Array.from(elements).forEach(function (element) {
-    element.addEventListener('click', activateAR)
-  })
+
 
 }
+
+const activateAR = () => {
+  viewer.activateAR()
+}
+Array.from(elements).forEach(function (element) {
+  element.addEventListener('click', activateAR)
+})
