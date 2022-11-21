68d44ff47233d9b6e02c91eaa99ab5b2358e3ce9
kweku moses <kweku.moses@hotmail.com>
2022-11-21 23:42:25 +0100

    Test

--- public/scripts/fetchUserData.js
+++ public/scripts/fetchUserData.js
@@ -10,25 +10,25 @@ const activateAR = (button) => {
 }
 
 const fetchUserData = async (e, product, color) => {
-  const request = await fetch('https://ipinfo.io/json?token=48c7527aaa89bb')
-  const jsonResponse = await request.json()
-  let city = jsonResponse.city
-  let country = jsonResponse.country
-  let userData = jsonResponse
-  const data = { city, country, product, userData }
+  // const request = await fetch('https://ipinfo.io/json?token=48c7527aaa89bb')
+  // const jsonResponse = await request.json()
+  // let city = jsonResponse.city
+  // let country = jsonResponse.country
+  // let userData = jsonResponse
+  // const data = { city, country, product, userData }
 
-  const options = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(data)
+  // const options = {
+  //   method: 'POST',
+  //   headers: {
+  //     'Content-Type': 'application/json'
+  //   },
+  //   body: JSON.stringify(data)
 
-  }
+  // }
 
-  fetch('/', options).then((response) => {
-    viewer.activateAR()
-  })
+  // fetch('/', options).then((response) => {
+  // })
+  viewer.activateAR()
 
   // try {
   //   const fetchResponse = await fetch(`/`, options);
