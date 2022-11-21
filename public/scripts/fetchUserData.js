72f940ff30bbfc43754ea888f03f879b500c39c4
kweku moses <kweku.moses@hotmail.com>
2022-11-21 23:16:40 +0100

    Test then and e.target

--- public/scripts/fetchUserData.js
+++ public/scripts/fetchUserData.js
@@ -1,10 +1,12 @@
-const fetchUserData = async (product, color) => {
+const fetchUserData = async (e, product, color) => {
+  let button = e.target.element
   const request = await fetch('https://ipinfo.io/json?token=48c7527aaa89bb')
   const jsonResponse = await request.json()
   let city = jsonResponse.city
   let country = jsonResponse.country
   let userData = jsonResponse
   const data = { city, country, product, userData }
+
   const options = {
     method: 'POST',
     headers: {
@@ -17,11 +19,24 @@ const fetchUserData = async (product, color) => {
 
   try {
     const fetchResponse = await fetch(`/`, options);
-    const data = await fetchResponse.json();
+    const data = await fetchResponse.json().then(() =>
+      activateAR(button)
+    );
     return data;
   } catch (e) {
     return e;
   }
 
-
 }
+let viewer = document.getElementById('the-viewer')
+let elements = document.getElementsByClassName('item-drop')
+
+Array.from(elements).forEach(function (element) {
+  element.addEventListener('click', activateAR)
+})
+
+
+const activateAR = (button) => {
+  button.activateAR()
+}
+
