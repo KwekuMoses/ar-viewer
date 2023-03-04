7d141eabc53f4ad208643972a0f27cc8ba2c913b
Kweku Moses <kweku.moses@hotmail.com>
2023-03-04 15:36:41 +0100

    Fix url

--- app.js
+++ app.js
@@ -29,8 +29,8 @@ app.get('/', (req, res) => {
 
   let product = req.query.product
 
-  let iphoneSrc = 'https://still-lake-53402.herokuapp.com/ ' + catalog + product + '.usdz'
-  let androidSrc = 'https://still-lake-53402.herokuapp.com/' + catalog + product + '.glb'
+  let iphoneSrc = 'https://still-lake-53402.herokuapp.com/ ' + catalog + '/' + product + '.usdz'
+  let androidSrc = 'https://still-lake-53402.herokuapp.com/' + catalog + '/' + product + '.glb'
 
   let buttonText = req.query.buttonText
   let buttonBorder = req.query.buttonBorder
