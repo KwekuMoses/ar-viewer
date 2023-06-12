fd21d4f482435e82e8b4796e7fcfcebf8e369124
kweku <kweku.moses@hotmail.com>
2023-06-12 21:16:09 +0200

    Add spaces in styling.js

--- public/scripts/styling.js
+++ public/scripts/styling.js
@@ -4,21 +4,27 @@ let btntext = document.getElementById('btntext')
 let icon = document.getElementById('theIcon')
 let color = thebutton.getAttribute('background-color')
 thebutton.style.backgroundColor = color
+
 // style the border
 let border = thebutton.getAttribute('button-border')
 thebutton.style.border = border
+
 // style the text-color
 let textColor = thebutton.getAttribute('text-color')
 thebutton.style.color = textColor
+
 // style the width
 let button_width = thebutton.getAttribute('width')
 thebutton.style.width = button_width
+
 // style the height
 let button_height = thebutton.getAttribute('height')
 thebutton.style.height = button_height
+
 // style the fontsize
 let fontSize = btntext.getAttribute('fontSize')
 btntext.style.fontSize = fontSize
+
 // style the iconWidth
 let iconWidth = theIcon.getAttribute('iconWidth')
 icon.style.width = iconWidth
